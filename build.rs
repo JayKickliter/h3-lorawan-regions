7eead0c263bfc4fc482b3e46e6d9f221f8253b82
Jay Kickliter <jay@kickliter.com>
2022-09-08 15:53:56 -0600

    Put every region behind a feature

--- build.rs
+++ build.rs
@@ -1,4 +1,3 @@
-extern crate byteorder;
 use byteorder::{ReadBytesExt, LE};
 use std::{
     fs::File,
@@ -7,64 +6,98 @@ use std::{
 };
 
 fn main() {
+    #[cfg(feature = "compact")]
     let compact_regions = [
+        #[cfg(feature = "AS923_1")]
         ("AS923_1", "src/compact/AS923-1.res7.h3idx"),
+        #[cfg(feature = "AS923_1B")]
         ("AS923_1B", "src/compact/AS923-1B.res7.h3idx"),
+        #[cfg(feature = "AS923_2")]
         ("AS923_2", "src/compact/AS923-2.res7.h3idx"),
+        #[cfg(feature = "AS923_3")]
         ("AS923_3", "src/compact/AS923-3.res7.h3idx"),
+        #[cfg(feature = "AS923_4")]
         ("AS923_4", "src/compact/AS923-4.res7.h3idx"),
+        #[cfg(feature = "AU915")]
         ("AU915", "src/compact/AU915.res7.h3idx"),
+        #[cfg(feature = "CN470")]
         ("CN470", "src/compact/CN470.res7.h3idx"),
+        #[cfg(feature = "EU433")]
         ("EU433", "src/compact/EU433.res7.h3idx"),
+        #[cfg(feature = "EU868")]
         ("EU868", "src/compact/EU868.res7.h3idx"),
+        #[cfg(feature = "IN865")]
         ("IN865", "src/compact/IN865.res7.h3idx"),
+        #[cfg(feature = "KR920")]
         ("KR920", "src/compact/KR920.res7.h3idx"),
+        #[cfg(feature = "RU864")]
         ("RU864", "src/compact/RU864.res7.h3idx"),
+        #[cfg(feature = "US915")]
         ("US915", "src/compact/US915.res7.h3idx"),
     ];
 
+    #[cfg(feature = "nocompact")]
     let nocompact_regions = [
+        #[cfg(feature = "AS923_1")]
         ("AS923_1", "src/nocompact/AS923-1.res7.h3idx"),
+        #[cfg(feature = "AS923_1B")]
         ("AS923_1B", "src/nocompact/AS923-1B.res7.h3idx"),
+        #[cfg(feature = "AS923_2")]
         ("AS923_2", "src/nocompact/AS923-2.res7.h3idx"),
+        #[cfg(feature = "AS923_3")]
         ("AS923_3", "src/nocompact/AS923-3.res7.h3idx"),
+        #[cfg(feature = "AS923_4")]
         ("AS923_4", "src/nocompact/AS923-4.res7.h3idx"),
+        #[cfg(feature = "AU915")]
         ("AU915", "src/nocompact/AU915.res7.h3idx"),
+        #[cfg(feature = "CN470")]
         ("CN470", "src/nocompact/CN470.res7.h3idx"),
+        #[cfg(feature = "EU433")]
         ("EU433", "src/nocompact/EU433.res7.h3idx"),
+        #[cfg(feature = "EU868")]
         ("EU868", "src/nocompact/EU868.res7.h3idx"),
+        #[cfg(feature = "IN865")]
         ("IN865", "src/nocompact/IN865.res7.h3idx"),
+        #[cfg(feature = "KR920")]
         ("KR920", "src/nocompact/KR920.res7.h3idx"),
+        #[cfg(feature = "RU864")]
         ("RU864", "src/nocompact/RU864.res7.h3idx"),
+        #[cfg(feature = "US915")]
         ("US915", "src/nocompact/US915.res7.h3idx"),
     ];
 
-    let out_path: PathBuf = [std::env::var("OUT_DIR").unwrap().as_str(), "compact.rs"]
-        .iter()
-        .collect();
-    let mut out = BufWriter::new(File::create(out_path).unwrap());
+    #[cfg(feature = "compact")]
+    {
+        let out_path: PathBuf = [std::env::var("OUT_DIR").unwrap().as_str(), "compact.rs"]
+            .iter()
+            .collect();
+        let mut out = BufWriter::new(File::create(out_path).unwrap());
 
-    gen_region_header(&compact_regions, &mut out);
+        gen_region_header(&compact_regions, &mut out);
 
-    for (region_name, h3_file_path) in compact_regions.iter() {
-        let h3_file_path = Path::new(h3_file_path).canonicalize().unwrap();
-        println!("cargo:rerun-if-changed={}", h3_file_path.to_str().unwrap());
-        let h3_file = BufReader::new(File::open(h3_file_path).unwrap());
-        gen_region_array(region_name, h3_file, &mut out);
+        for (region_name, h3_file_path) in compact_regions.iter() {
+            let h3_file_path = Path::new(h3_file_path).canonicalize().unwrap();
+            println!("cargo:rerun-if-changed={}", h3_file_path.to_str().unwrap());
+            let h3_file = BufReader::new(File::open(h3_file_path).unwrap());
+            gen_region_array(region_name, h3_file, &mut out);
+        }
     }
 
-    let out_path: PathBuf = [std::env::var("OUT_DIR").unwrap().as_str(), "nocompact.rs"]
-        .iter()
-        .collect();
-    let mut out = BufWriter::new(File::create(out_path).unwrap());
+    #[cfg(feature = "nocompact")]
+    {
+        let out_path: PathBuf = [std::env::var("OUT_DIR").unwrap().as_str(), "nocompact.rs"]
+            .iter()
+            .collect();
+        let mut out = BufWriter::new(File::create(out_path).unwrap());
 
-    gen_region_header(&nocompact_regions, &mut out);
+        gen_region_header(&nocompact_regions, &mut out);
 
-    for (region_name, h3_file_path) in nocompact_regions.iter() {
-        let h3_file_path = Path::new(h3_file_path).canonicalize().unwrap();
-        println!("cargo:rerun-if-changed={}", h3_file_path.to_str().unwrap());
-        let h3_file = BufReader::new(File::open(h3_file_path).unwrap());
-        gen_region_array(region_name, h3_file, &mut out);
+        for (region_name, h3_file_path) in nocompact_regions.iter() {
+            let h3_file_path = Path::new(h3_file_path).canonicalize().unwrap();
+            println!("cargo:rerun-if-changed={}", h3_file_path.to_str().unwrap());
+            let h3_file = BufReader::new(File::open(h3_file_path).unwrap());
+            gen_region_array(region_name, h3_file, &mut out);
+        }
     }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
-pub mod compact{
+#[cfg(feature = "compact")]
+pub mod compact {
     include!(concat!(env!("OUT_DIR"), "/compact.rs"));
 }
 
+#[cfg(feature = "nocompact")]
 pub mod nocompact {
     include!(concat!(env!("OUT_DIR"), "/nocompact.rs"));
 }
